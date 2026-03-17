67860692b3db4cfc90bb2a0f0c4be1c70a972d24
Andreas Alexander Vannebo <andrava@stud.ntnu.no>
2026-03-17 10:46:38 +0100

    changes names

--- src/qube_description/setup.py
+++ src/qube_description/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'cube_description'
+package_name = 'qube_description'
 
 setup(
     name=package_name,
