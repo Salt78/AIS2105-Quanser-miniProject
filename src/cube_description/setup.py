94e3e10fba7fd57dfaf897bb9414124680ad06df
Andreas Alexander Vannebo <andrava@stud.ntnu.no>
2026-03-17 10:38:27 +0100

    Copied stuff to cube_decription

--- src/cube_description/setup.py
+++ src/cube_description/setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'cube_description'
+package_name = 'joint_description'
 
 setup(
     name=package_name,
@@ -10,13 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', 'urdf.rviz'))),
+    ],  
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='andreas',
     maintainer_email='andrava@stud.ntnu.no',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='Apache-2.0',
     extras_require={
         'test': [
             'pytest',
