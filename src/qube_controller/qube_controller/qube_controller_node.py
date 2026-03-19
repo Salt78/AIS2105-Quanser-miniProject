2e43b9b79be487c1acd86f08f6fd765a58f28a00
haakosag <haakon.sagmo@outlook.com>
2026-03-19 13:21:18 +0100

    updated qube controller

--- src/qube_controller/qube_controller/qube_controller_node.py
+++ src/qube_controller/qube_controller/qube_controller_node.py
@@ -1,13 +1,31 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState 
 from rcl_interfaces.msg import SetParametersResult
 import numpy as np
 
 
 class QubeControllerNode(Node):
-     def __init__(self):
-          super().__init__('qube_controller_node')
+    def __init__(self):
+        super().__init__('qube_controller_node')
+
+        self.joint_state_sub = self.create_subscription(
+            JointState,
+            '/joint_states',
+            self.joint_state_callback,
+            10
+        )
+
+        self.latest_joint_state = None
+
+    
+    def joint_state_callback(self, msg: JointState):
+        # store or process joint states; head: [q1, q2], vel: [dq1, dq2], effort maybe
+        self.latest_joint_state = msg
+        self.get_logger().debug(
+            f"joint_states received position={msg.position} velocity={msg.velocity} effort={msg.effort}"
+        )
 
     
     
