250b95baa744a7e466877d10b48554e8c905c3b1
Andreas Alexander Vannebo <andrava@stud.ntnu.no>
2026-04-01 10:49:35 +0200

    fixed launch arguments for xacro

--- src/qube_bringup/launch/bringup.launch.py
+++ src/qube_bringup/launch/bringup.launch.py
@@ -1,33 +1,47 @@
 from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+import xacro
 
 xacro_file = os.path.join(get_package_share_directory("qube_bringup"),"urdf","controlled_qube.urdf.xacro")
 
 launch_dir = PathJoinSubstitution([FindPackageShare('qube_driver'), 'launch'])
 
+def launch_setup(context, *args, **kwargs):
+    simulation = LaunchConfiguration('simulation').perform(context)
+    baud_rate = LaunchConfiguration('baud_rate').perform(context)
+    device = LaunchConfiguration('device').perform(context)
+
+    xacro_doc = xacro.process_file(
+        xacro_file,
+        mappings={
+            'simulation': simulation,
+            'baud_rate': baud_rate,
+            'device': device
+        }
+    )
+
+    robot_description = {'robot_description': xacro_doc.toxml()}
+    return [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[robot_description],
+            output='screen'
+        )
+    ]
+
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('baud_rate', default_value='115200'),
         DeclareLaunchArgument('device', default_value='/dev/ttyACM0'),
-        DeclareLaunchArgument('simulator', default_value='false'),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': Command([ #Processes Xacro while passing launch parameters
-                'xacro', ' ',
-                xacro_file, ' ',
-                'baud_rate:=', LaunchConfiguration('baud_rate'), ' ',
-                'device:=', LaunchConfiguration('device'), ' ',
-                'simulator:=', LaunchConfiguration('simulator')
-            ])}]
-        ),
+        DeclareLaunchArgument('simulation', default_value='false'),
+        OpaqueFunction(function=launch_setup),
         Node(
             package='rviz2',
             executable='rviz2',
